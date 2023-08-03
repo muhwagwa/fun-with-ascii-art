3e1a4e7995ba39de300abe914b82401cfcdeedf7
나혜주 <nahyeju@nahyejus-MacBook-Air.local>
2023-08-03 16:55:05 -0500

    add helper function and constants file

---
+++ constants.py
@@ -0,0 +1,25 @@
+PERMITTED_EXTENSION = ["png", "jpg", "jpeg"]
+HEADER = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <style>
+            body{
+                line-height: 6.5px;
+                letter-spacing: 3px;
+                font-family: 'Courier', sans-serif;
+                font-size: 7px;
+                white-space: nowrap;
+                background-color: lightgray;
+            }
+        </style>
+        <title>Page Title</title>
+        </head>
+        <body>
+
+        <h1>ASCII Art</h1>
+    """
+FOOTER = """
+        </body>
+        </html>
+    """
