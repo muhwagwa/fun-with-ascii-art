4ab64cf643bc87f177864d82d81090585f34bf93
나혜주 <nahyeju@nahyejus-MacBook-Air.local>
2023-08-06 01:34:49 -0500

    fix lint error for tile.py

--- tile.py
+++ tile.py
@@ -1,9 +1,32 @@
+"""Tile
+2x2 Tile object that will be converted to 1 ascii char.
+
+This file can also be imported as a module and contains the following functions:
+
+    * create_check_array : Create an array that holds shape info for a 2x2 tile
+    * print_terminal : Print alphabet to terminal with matching color
+    * convert_to_char : Convert 2x2 tile info to ascii char
+"""
 from colorama import Fore, Style
 import tables
 
 
 class Tile:
-    def __init__(self, input_array):
+    """2x2 Tile object that will be converted to 1 ascii char.
+
+    Attributes:
+        input_array (list): List of initial cell values.
+        color_array (list): List of rgb values of the cells in the tile.
+        check (list): List of binary cell values.
+        count0 (int): No. of cells with value 0.
+        count70 (int): No. of cells with value 70.
+        count140 (int): No. of cells with value 140.
+        count210 (int): No. of cells with value 210.
+        max_frequency (int): Maximum frequency of the cell values in a tile.
+        chosen (int): Cell value with maximum frequency.
+    """
+
+    def __init__(self, input_array: list):
         self.input_array = input_array
         self.color_array = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.check = [0, 0, 0, 0]
@@ -15,6 +38,7 @@ class Tile:
         self.chosen = 0
 
     def create_check_array(self):
+        """Create an array that holds shape info for a 2x2 tile"""
         for elem in self.input_array:
             match elem:
                 case 0:
@@ -43,7 +67,12 @@ class Tile:
             if elem == self.chosen:
                 self.check[index] = 1
 
-    def print_terminal(self, alphabet):
+    def print_terminal(self, alphabet: str):
+        """Print alphabet to terminal with matching color
+
+        Args:
+            alphabet (str): Alphabet to print to terminal
+        """
         match self.chosen:
             case 0:
                 print(Fore.BLACK + Style.NORMAL + alphabet, end="")
@@ -58,12 +87,15 @@ class Tile:
                 print(Fore.WHITE + Style.BRIGHT + alphabet, end="")
                 print(Style.RESET_ALL, end="")
 
-    def convert_to_terminal(self):
-        self.create_check_array()
-        alphabet = tables.default(self.check)
-        self.print_terminal(alphabet)
+    def convert_to_char(self, style: str):
+        """Convert 2x2 tile info to ascii char
 
-    def convert_to_char(self, style):
+        Args:
+            style (str): Desired style of Ascii converting.
+
+        Returns:
+            str: result ascii char of the tile
+        """
         self.create_check_array()
         match style:
             case "terminal":
